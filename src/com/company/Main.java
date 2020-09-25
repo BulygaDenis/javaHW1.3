7f6338b36e9b44590766289b3af13a2593187b13
Neposeda644 <denis_bulyga@mail.ru>
2020-09-25 16:19:14 +0300

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        int balance = 2_000_000_000;
+        int change = 500_000_000;
+        int total = balance - change;
+        System.out.println(total);
+    }
+}
